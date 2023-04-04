fcaa77c4c546cb8f32340ba20aedcc35775bf262
Saharsh Cherian <saharshdc@gmail.com>
2023-04-04 09:39:49 -0400

    completed bck end and initialized react front end

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
-
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts} from './data/index.js';
 
 // configurations for middleware
 const __filename = fileURLToPath(import.meta.url);
@@ -57,6 +59,11 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     }).then(() =>{
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+        // Seed the database Once
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+
     }
     )
 .catch((error) => console.log(`${error} did not connect`));
